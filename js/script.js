ca255228d5830796a845508dc7c222501b26fb8c
LamNicole <nicole.lam@yale.edu>
2025-05-29 06:52:31 -0400

    fix logo and homepage

--- js/script.js
+++ js/script.js
@@ -57,10 +57,9 @@ $(window).load(function() {
 
 
 $(function() {
-
-	$('a[href*=#]:not([href=#])').click(function() {
+	// Only handle links with hash symbols for smooth scrolling
+	$('a[href*="#"]:not([href="#"])').click(function() {
 		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
-
 			var target = $(this.hash);
 			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 			if (target.length) {
@@ -71,7 +70,6 @@ $(function() {
 			}
 		}
 	});
-
 });
 
 /******************************************************************************************************************************
